10909f3da9918a6d8a7736e491d69b6ce609e530
LuanG15 <luan26gomes@gmail.com>
2023-06-13 17:26:04 -0300

    v4

--- js/script.js
+++
@@ -1,9 +0,0 @@
-var offset = $('#menu').offset().top;
-var $menu = $('#menu');
-$(document).on('scroll', function () {
-    if (offset <= $(window).scrollTop()) {
-        $menu.addClass('fixar');
-    } else {
-        $menu.removeClass('fixar');
-    }
-});
